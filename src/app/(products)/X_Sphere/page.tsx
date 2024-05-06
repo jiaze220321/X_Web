0a1930af3daa0d17ed3b974f839e180998381506
jiaze220321 <zjz20190829@163.com>
2024-05-06 11:59:54 +0800

    feat: 新建X LAB，X Man，X Edge，X Sphere页面

---
+++ src/app/(products)/X_Sphere/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20vh', fontSize: '24px', fontWeight: 'bold' }}>
+      Hello, welcome to X Sphere!
+    </div>
+  );
+}
+
+export default App;
